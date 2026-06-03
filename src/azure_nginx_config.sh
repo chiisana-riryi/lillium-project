c0f876ea2a31dd6e15b4ecc4e35c0d32d0410f1e
work-laptop <->
2026-06-03 14:59:34 +0700

    add nginx config command

---
+++ src/azure_nginx_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat > /etc/nginx/sites-available/default <<'EOF'
+server {
+  listen 8080;
+  root /home/site/wwwroot/public;
+  index index.php index.html;
+
+  location / {
+    try_files $uri $uri/ /index.php?$query_string;
+  }
+
+  location ~ \.php$ {
+    include fastcgi_params;
+    fastcgi_pass 127.0.0.1:9000;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  }
+}
+EOF
+
+service nginx restart
